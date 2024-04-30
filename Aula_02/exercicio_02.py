c0c9bbcf0a53fe05bf7cf00340c4ce84be421b12
Renato <jrenato.sabino@gmail.com>
2024-04-30 08:44:29 -0300

    Upload exercicio 02

--- Aula_02/exercicio_02.py
+++ Aula_02/exercicio_02.py
@@ -1,8 +1,8 @@
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
 
 try:
-    num = float(input("Insira o primeiro número: "))
+    num = int(input("Insira o primeiro número inteiro: "))
     resto = num % 5
     print(f"O resto da divisão é {resto}")
 except:
-    print("O valor digitado não é um número!")
+    print("O valor digitado não é um número inteiro!")
