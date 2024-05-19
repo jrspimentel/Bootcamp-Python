3fae5259e57fb524c98e8f9d471ec466dbc3f042
Renato <jrenato.sabino@gmail.com>
2024-05-19 12:04:08 -0300

    Adicionando exercício 19

---
+++ Aula_02/exercicio_19.py
@@ -0,0 +1,9 @@
+# 19. Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
+
+# Input de dados
+num_1 = float(input("Insira o primeiro número: "))
+num_2 = float(input("Insira o segundo número: "))
+
+comparacao = num_1 == num_2
+
+print(f"Os números {num_1} e {num_2} são iguais? {comparacao}")
