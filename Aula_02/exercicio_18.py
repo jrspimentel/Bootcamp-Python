1350370efb15a3d6397bcafe9b739568cbe7b715
Renato <jrenato.sabino@gmail.com>
2024-05-19 11:59:24 -0300

    Adicionando exercício 18

---
+++ Aula_02/exercicio_18.py
@@ -0,0 +1,6 @@
+# 18. Desenvolva um programa que peça ao usuário para inserir um valor booleano e, em seguida, inverta esse valor.
+
+valor1 = True
+valor2 = False
+resultado = not valor1
+print("Resultado do OR lógico:", resultado)
