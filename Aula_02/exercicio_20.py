5d13d9bd38d68b529b2d0f330c7bd44f728244c9
Renato <jrenato.sabino@gmail.com>
2024-05-19 12:06:03 -0300

    Adicionando exercício 20

---
+++ Aula_02/exercicio_20.py
@@ -0,0 +1,7 @@
+# 20. Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes.
+
+# Input de dados
+num_1 = float(input("Insira o primeiro número: "))
+num_2 = float(input("Insira o segundo número: "))
+comparacao = num_1 != num_2
+print(f"Os números {num_1} e {num_2} são diferentes? {comparacao}")
