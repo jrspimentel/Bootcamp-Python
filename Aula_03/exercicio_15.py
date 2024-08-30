5ade882603423ad5f729e4fa048b5bf1de2e5e4b
Renato Pimentel <jrenato.sabino@gmail.com>
2024-08-30 12:49:17 -0300

    update exercicio 15

---
+++ Aula_03/exercicio_15.py
@@ -0,0 +1,11 @@
+### Exercício 15. Processamento de Dados com Condição de Parada
+# Processar itens de uma lista até encontrar um valor específico que indica a parada..
+
+lista =  [1, 2, 3 , 4, "parar", 5, 6]
+valor = 0
+while valor < len(lista):
+    if lista[valor] == "parar":
+        print("Parada encontrada, processamento encerrado")
+        break
+    print(f"Processando valor: {lista[valor]}")
+    valor +=1
