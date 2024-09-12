ca676b31cb03a3b00ffda6d77c004741340d634a
Renato Pimentel <jrenato.sabino@gmail.com>
2024-09-12 08:37:00 -0300

    Adicionado exercício 06

---
+++ Aula_04/exercicio_06.py
@@ -0,0 +1,7 @@
+'''
+6. Eliminação de Duplicatas
+ Objetivo: Dada uma lista de emails, remover todos os duplicados.
+ '''
+emails = ["user@example.com", "admin@example.com", "user@example.com", "manager@example.com"]
+emails_unicos = set(emails)
+print(emails_unicos)
